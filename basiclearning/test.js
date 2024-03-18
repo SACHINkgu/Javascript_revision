c48b999e5470585c437f90a40d617c134a2b59e7
Sachin Kumar Gupta <90194058+SACHINkgu@users.noreply.github.com>
2024-03-18 19:55:54 +0000

    update

--- basiclearning/test.js
+++ basiclearning/test.js
@@ -1,6 +1,17 @@
-console.log("sachin")
+// console.log("sachin")
 
-console.log(2+2)
-let gameCounter = 100
-++gameCounter;
-console.log(gameCounter);
+// console.log(2+2)
+
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+//output
+// 12
+// 12
+// 122
+// 32
