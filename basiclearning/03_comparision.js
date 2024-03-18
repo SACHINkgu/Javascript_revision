0f4f98dbabaffd71b0e7ce4eeea68853079085a7
Sachin Kumar Gupta <90194058+SACHINkgu@users.noreply.github.com>
2024-03-18 20:08:52 +0000

    comparision concept

---
+++ basiclearning/03_comparision.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0); //checks only value
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===  strictly check (value and datatype)
+
+console.log("2" === 2);
