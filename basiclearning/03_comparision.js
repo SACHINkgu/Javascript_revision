553d0e306b2112068b49a98d86dabf67fe9c9c5c
Sachin Kumar Gupta <90194058+SACHINkgu@users.noreply.github.com>
2024-03-20 21:53:01 +0000

    more clearty

--- basiclearning/03_comparision.js
+++ basiclearning/03_comparision.js
@@ -8,12 +8,12 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 
-console.log(null > 0);
+console.log(null > 0);  //1:32:37 
 console.log(null == 0); //checks only value
-console.log(null >= 0);
+console.log(null >= 0); // here in this , null is converted in 0
 
 console.log(undefined == 0);
-console.log(undefined > 0);  
+console.log(undefined > 0);  //undefined gives false in each of these case.
 console.log(undefined < 0);
 // avoid these type of comparision bez they create confusion .
 
