c98839a18f63620a32e07212c5080fef97c207c9
Sachin Kumar Gupta <90194058+SACHINkgu@users.noreply.github.com>
2024-03-20 21:30:33 +0000

    updated the comment

--- basiclearning/03_comparision.js
+++ basiclearning/03_comparision.js
@@ -13,8 +13,9 @@ console.log(null == 0); //checks only value
 console.log(null >= 0);
 
 console.log(undefined == 0);
-console.log(undefined > 0);
+console.log(undefined > 0);  
 console.log(undefined < 0);
+// avoid these type of comparision bez they create confusion .
 
 // ===  strictly check (value and datatype)
 
